5819bb7500a214e0d310e4d6dacc241cbb4cf64d
Alena <jijiaxin.a@gmail.com>
2024-02-07 17:42:45 +0100

    Last touches interface

--- Running Python/User_interface.py
+++ Running Python/User_interface.py
@@ -1,4 +1,4 @@
-# User Interface
+# User Interface - the runner of our whole project
 
 # Example first input:
    # "C:/Users/Sedláček/pr/Project-Weather-electricity-prices"
@@ -9,36 +9,34 @@ import tkinter
 import pandas as pd
 import sys
 import os
+import tkinter.font as tkFont
 
+# to process inserted path - get rid off the wrong backslash
 def on_set():
     global directory_path
     directory_path = entry.get().strip('\"')
     app.destroy()
 
-# Set the theme and scaling before creating the main window
+# Setting the theme and scaling before creating the main window
 customtkinter.set_appearance_mode("dark")  # Other options: "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Other options like "dark-blue", "green"
-
-# Create the main window
 app = customtkinter.CTk()
 app.geometry('400x300')
 app.title("Set Working Directory")
 
-# Create a label with your chosen text
+# Main text for the first GUI
 label_text = """Please enter your working directory,
 where you have downloaded our GitHub repository.
 Then press the Set buttom and the working directory
 will be set for further usage."""
-label = customtkinter.CTkLabel(app, text=label_text)
+font_style_label = customtkinter.CTkFont(family="Times New Roman", size=15)
+label = customtkinter.CTkLabel(app, text=label_text, font=font_style_label)
 label.pack(pady=10)
 
 entry = customtkinter.CTkEntry(app)
 entry.pack(pady=10)
-
 set_button = customtkinter.CTkButton(app, text="Set", command=on_set)
 set_button.pack(pady=10)
-
-
 app.mainloop()
 
 if directory_path:  # Check if directory_path was set
@@ -47,14 +45,12 @@ if directory_path:  # Check if directory_path was set
 else:
     print("No directory path was set.")
 
-
-
 new_directory = directory_path
-#new_directory = "c:/Users/jijia/OneDrive/Desktop/Project_ python/Project-Weather-electricity-prices/Processing_and_graphs"
 
 # Changing the current working directory
 os.chdir(new_directory)
 
+# importing needed modules from other files
 from Processing_and_graphs.Searching_diff import searching_difference_diff
 from Processing_and_graphs.Searching_normal import searching_difference_normal
 from Processing_and_graphs.Data_preparation import Data_prep
@@ -78,7 +74,7 @@ customtkinter.set_ctk_parent_class(tkinter.Tk)
 customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-# Don't touch, seting default
+# Don't touch, setting default
 final_data = 'Data/final_data.csv'
 weather_new = 'Data/Weather_new.csv'
 weather_base = 'Data/Weather_base.xlsx'
@@ -89,7 +85,7 @@ output_file_path = 'Data/final_data.csv'
 
 input_row = []
 
-
+# the main running fuction
 def retrieve_input():
     source = optionmenu_1.get()
 
@@ -174,16 +170,15 @@ def retrieve_input():
     root.destroy()
 
 
+# Adjusting the graphical part of GUI
 root = customtkinter.CTk()
 root.geometry("700x780")
-
-
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=5, padx=60, fill="both", expand=True)
+font_style_head= customtkinter.CTkFont(family="Times New Roman", size=14, weight="bold")
+font_style = customtkinter.CTkFont(family="Times New Roman", size=9)
 
-label_1 = customtkinter.CTkLabel(master=frame, text = "Please insert seven temperature values in Celsius and the threshold value")
-label_1.pack(pady=10, padx=20)
-
+#Setting the entry bubbles
 entry_var_1 = tkinter.StringVar()
 entry_var_2 = tkinter.StringVar()
 entry_var_3 = tkinter.StringVar()
@@ -193,70 +188,98 @@ entry_var_6 = tkinter.StringVar()
 entry_var_7 = tkinter.StringVar()
 entry_var_threshold = tkinter.StringVar()
 
+# For the 1st temperature value 
+label_1 = customtkinter.CTkLabel(master=frame, text = "Please insert seven temperature values in Celsius and the threshold value")
+label_1.pack(pady=5, padx=5)
+label_1.configure(font=font_style_head)
 label_entry_1 = customtkinter.CTkLabel(master=frame, text="1st temperature (°C):")
-label_entry_1.pack(pady=(1,0), padx=10)
+label_entry_1.pack(pady=(0,0), padx=5)
+label_entry_1.configure(font=font_style)
 entry_1 = customtkinter.CTkEntry(master=frame, textvariable= entry_var_1)
-entry_1.pack(pady=3, padx=10)
+entry_1.pack(pady=0.5, padx=5)
+entry_1.configure(font=font_style)
 
+# For the 2nd temperature value 
 label_entry_2 = customtkinter.CTkLabel(master=frame, text="2nd temperature (°C):")
-label_entry_2.pack(pady=(1,0), padx=10)
+label_entry_2.pack(pady=(0,0), padx=5)
+label_entry_2.configure(font=font_style)
 entry_2 = customtkinter.CTkEntry(master=frame, textvariable= entry_var_2)
-entry_2.pack(pady=3, padx=10)
+entry_2.pack(pady=0.5, padx=5)
+entry_2.configure(font=font_style)
 
-
+# For the 3rd temperature value 
 label_entry_3 = customtkinter.CTkLabel(master=frame, text="3rd temperature (°C):")
-label_entry_3.pack(pady=(1,0), padx=10)
+label_entry_3.pack(pady=(0,0), padx=5)
+label_entry_3.configure(font=font_style)
 entry_3 = customtkinter.CTkEntry(master=frame, textvariable= entry_var_3)
-entry_3.pack(pady=3, padx=10)
+entry_3.pack(pady=0.5, padx=5)
+entry_3.configure(font=font_style)
 
-
+# For the 4th temperature value 
 label_entry_4 = customtkinter.CTkLabel(master=frame, text="4st temperature (°C):")
-label_entry_4.pack(pady=(1,0), padx=10)
+label_entry_4.pack(pady=(0,0), padx=5)
+label_entry_4.configure(font=font_style)
 entry_4 = customtkinter.CTkEntry(master=frame, textvariable= entry_var_4)
-entry_4.pack(pady=3, padx=10)
+entry_4.pack(pady=0.5, padx=5)
+entry_4.configure(font=font_style)
 
-
+# For the 5th temperature value 
 label_entry_5 = customtkinter.CTkLabel(master=frame, text="5st temperature (°C):")
-label_entry_5.pack(pady=(1,0), padx=10)
+label_entry_5.pack(pady=(0,0), padx=5)
+label_entry_5.configure(font=font_style)
 entry_5 = customtkinter.CTkEntry(master=frame, textvariable= entry_var_5)
-entry_5.pack(pady=3, padx=10)
+entry_5.pack(pady=0.5, padx=5)
+entry_5.configure(font=font_style)
 
-
+# For the 6th temperature value 
 label_entry_6 = customtkinter.CTkLabel(master=frame, text="6st temperature (°C):")
-label_entry_6.pack(pady=(1,0), padx=10)
+label_entry_6.pack(pady=(0,0), padx=5)
+label_entry_6.configure(font=font_style)
 entry_6 = customtkinter.CTkEntry(master=frame, textvariable= entry_var_6)
-entry_6.pack(pady=3, padx=10)
+entry_6.pack(pady=0.5, padx=5)
+entry_6.configure(font=font_style)
 
-
+# For the 7th temperature value 
 label_entry_7 = customtkinter.CTkLabel(master=frame, text="7st temperature (°C):")
-label_entry_7.pack(pady=(1,0), padx=10)
+label_entry_7.pack(pady=(0,0), padx=5)
+label_entry_7.configure(font=font_style)
 entry_7 = customtkinter.CTkEntry(master=frame, textvariable= entry_var_7)
-entry_7.pack(pady=3, padx=10)
+entry_7.pack(pady=0.5, padx=5)
+entry_7.configure(font=font_style)
 
-
+# For the threshold 
 label_entry_thre = customtkinter.CTkLabel(master=frame, text="Threshold:")
-label_entry_thre.pack(pady=(3,0), padx=10)
+label_entry_thre.pack(pady=(0,0), padx=5)
+label_entry_thre.configure(font=font_style)
 entry_threshold = customtkinter.CTkEntry(master=frame, textvariable= entry_var_threshold)
-entry_threshold.pack(pady=5, padx=10)
+entry_threshold.pack(pady=0.5, padx=5)
+entry_threshold.configure(font=font_style)
 
-
+# For the list of possible sources that user can choose from
 optionmenu_1 = customtkinter.CTkOptionMenu(frame, values=["Generation off shore wind", 
             "Generation from other convential sources", "Generation from other renewables",
             "Generation on shore wind", "Generation from photovoltaics",
             "Generation from pumper storage", "Total electricity consumption"])
-optionmenu_1.pack(pady=10, padx=10)
+optionmenu_1.pack(pady=5, padx=5)
 optionmenu_1.set("Source")
+optionmenu_1.configure(font=font_style)
 
+# For the methods
 combobox_1 = customtkinter.CTkComboBox(frame, values=["Normal", "Differentiated"])
-combobox_1.pack(pady=10, padx=10)
+combobox_1.pack(pady=5, padx=5)
 combobox_1.set("Method")
+combobox_1.configure(font=font_style)
 
+#For the Graph
 combobox_2 = customtkinter.CTkComboBox(frame, values=["For each year", "Whole period"])
-combobox_2.pack(pady=10, padx=10)
+combobox_2.pack(pady=5, padx=5)
 combobox_2.set("Graph")
+combobox_2.configure(font=font_style)
 
+#Final FIND button
 button = customtkinter.CTkButton(master = frame, text = "Find", command= retrieve_input)
 button.pack(pady=12,padx=10)
+button.configure(font=font_style)
 
 root.mainloop()
  
